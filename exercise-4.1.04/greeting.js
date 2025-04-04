e4b89e8d76b010ddce4808bf290f347365509616
Mustafa Yildirim <mustafa.yildirim@powercoders.org>
2025-04-04 14:24:23 +0200

    Add all exercise updates

---
+++ exercise-4.1.04/greeting.js
@@ -0,0 +1,5 @@
+// Ask for the user's name
+let name = prompt("What is your name?");
+
+// Print a greeting message in the console
+console.log("Hello, " + name + "! Welcome to the world of JavaScript.");
